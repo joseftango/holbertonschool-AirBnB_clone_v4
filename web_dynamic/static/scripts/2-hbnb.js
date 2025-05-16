557043e2713cd01912b5480ce9235d45bbce1c9d
joseftango <4279@holbertonschool.com>
2025-05-16 17:04:48 +0100

    scripts

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,4 +18,15 @@ $(document).ready(function () {
 
         $('.amenities > h4').text(display);
     });
+
+    	$.get("http://127.0.0.1:5001/api/v1/status/", function(data, textStatus)
+	{
+		if(data.status == 'OK') {
+            $('div#api_status').addClass('available')
+        }
+        else {
+            $('div#api_status').removeClass('available')
+        }
+	});
+
 });
