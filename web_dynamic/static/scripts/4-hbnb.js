cfd1a6555520a2a028a66ba08b4b42e0f72b1586
joseftango <4279@holbertonschool.com>
2025-05-19 13:01:14 +0100

    web_dynamic

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,86 @@
+
+const checked_amenities = {};
+const needed_amenities = {};
+
+$(document).ready(function () {
+
+	$('input[type=checkbox]').click(function () {
+
+		if ($(this).is(':checked')) {
+			checked_amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      
+		} else {
+			delete checked_amenities[$(this).attr('data-id')];
+		}
+    
+        const names = Object.values(checked_amenities);
+        let display = names.slice(0, 3).join(', ');
+        if (display.length > 40) {
+            display = display.slice(0, 40)
+            display += ' ...';
+        }
+
+        $('.amenities > h4').text(display);
+
+        let ids = [];
+        ids.push(...Object.keys(checked_amenities));
+
+        if (ids) {
+          needed_amenities['amenities'] = ids;
+        }
+    });
+
+  $.get("http://127.0.0.1:5001/api/v1/status/", function(data, textStatus)
+    {
+      if(data.status == 'OK') {
+        $('div#api_status').addClass('available')
+      } else {
+        $('div#api_status').removeClass('available')
+      }
+    });
+
+  $('button').click(function() {
+   $.ajax({
+    url: 'http://127.0.0.1:5001/api/v1/places_search',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(needed_amenities),
+    success: function(places) {
+    places.sort((a, b) => a.name.localeCompare(b.name));
+    // console.log(places.length)
+    places.forEach(place => {
+    const article = $(`
+                  <article>
+                    <div class="headline">
+                      <h2>${place.name}</h2>
+                      <div class="price_by_night">$${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                      <div class="max_guest">
+                        <div class="guest_icon"></div>
+                        <p>${place.max_guest} Guests</p>
+                      </div>
+                      <div class="number_rooms">
+                        <div class="bed_icon"></div>
+                        <p>${place.number_rooms} Bedrooms</p>
+                      </div>
+                      <div class="number_bathrooms">
+                        <div class="bath_icon"></div>
+                        <p>${place.number_bathrooms} Bathrooms</p>
+                      </div>
+                    </div>
+                    <div class="description">
+                      <p>${place.description}</p>
+                    </div>
+                  </article>`);
+    $('.places').append(article);
+    });
+    },
+    error: function(xhr, status, error) {
+      console.error('Error:', error);
+    }
+    });
+
+  });
+
+});
